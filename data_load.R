15536650791c7e4104eec448f291d68f72f2b330
Cooper <coopergee49@gmail.com>
2024-10-23 12:17:52 +1100

    Updated report and data load file, uploaded RBA cash rate sheet

--- data_load.R
+++ data_load.R
@@ -13,7 +13,7 @@ add_month_df <- function(month,year,rent_or_sales,lga_or_postcode) {
   
   colnames(sheet) <- NULL
   sheet <- sheet[-1, , drop=FALSE]
-  sheet$date <- as.Date(sprintf("30 %s %s",month,year),format="%d %B %Y")
+  sheet$date <- as.Date(sprintf("01 %s %s",month,year),format="%d %B %Y") + months(1) - days(1)
   sheet
 }
 
@@ -29,18 +29,30 @@ get_dataframes <- function(year_range,rent_or_sales,lga_or_postcode){
   final_df
 }
 
+clean_comma <- function(df){
+  new_df = as.numeric(gsub(",","",df))
+  new_df
+}
+clean_percentage <- function(df){
+  new_df = as.numeric(gsub("%","",df))
+  new_df
+}
+
 clean_rent_df <- function(rent_df){
   rent_column_names <- c("gmr","greater_sydney","rings","lga","dwelling_type","no_of_bedrooms","first_quartile_rent_price","median_rent_price","third_quartile_rent_price","no_of_bonds_new","no_of_bonds_total","qtly_change_rent","annual_change_rent","qtly_change_bonds_new","annual_change_bonds_new","date")
   colnames(rent_df) <- rent_column_names
   rent_df <- janitor::clean_names((rent_df))
-  rent_df[,7:15] <- lapply(rent_df[,7:15],as.numeric)
+  rent_df[,7:11] <- lapply(rent_df[,7:11],clean_comma)
+  rent_df[,12:15] <- lapply(rent_df[,12:15],clean_percentage)
   rent_df
 }
 
 clean_sales_df <- function(sales_df){
   sales_column_names <- c("gmr","greater_sydney","rings","lga","dwelling_type","first_quartile_sales_price","median_sales_price","third_quartile_sales_price","mean_sales_price","no_of_sales","qtly_change_price","annual_change_price","qtly_change_count","annual_change_count","date")
   colnames(sales_df) <- sales_column_names
-  sales_df[,6:14] <- lapply(sales_df[,6:14],as.numeric)
+  #sales_df[,6:14] <- as.numeric(gsub(",","",sales_df[,6:14]))14
+  sales_df[,6:10] <- lapply(sales_df[,6:10],clean_comma)
+  sales_df[,11:14] <- lapply(sales_df[,11:14],clean_percentage)
   sales_df
 }
 
