69a1167f6bba5d03cc1366e022b341308594c30b
Cooper <coopergee49@gmail.com>
2024-10-31 12:58:25 +1100

    Worked on report, modified the data_load file and added references to the bibtex

--- data_load.R
+++ data_load.R
@@ -50,14 +50,54 @@ clean_rent_df <- function(rent_df){
 clean_sales_df <- function(sales_df){
   sales_column_names <- c("gmr","greater_sydney","rings","lga","dwelling_type","first_quartile_sales_price","median_sales_price","third_quartile_sales_price","mean_sales_price","no_of_sales","qtly_change_price","annual_change_price","qtly_change_count","annual_change_count","date")
   colnames(sales_df) <- sales_column_names
-  #sales_df[,6:14] <- as.numeric(gsub(",","",sales_df[,6:14]))14
   sales_df[,6:10] <- lapply(sales_df[,6:10],clean_comma)
   sales_df[,11:14] <- lapply(sales_df[,11:14],clean_percentage)
   sales_df
 }
 
-sales_df <- get_dataframes(2018:2024,"sales","LGA") |> clean_sales_df()
-rents_df <- get_dataframes(2018:2024,"rent","LGA") |> clean_rent_df()
+sales_df <- get_dataframes(2018:2024,"sales","LGA")
+rents_df <- get_dataframes(2018:2024,"rent","LGA")
+
+sales_df = sales_df |> clean_sales_df()
+rents_df = rents_df |> clean_rent_df()
+
+# Get RBA cash rate data and merge into sales and rents dataframe
+
+RBA_cash_rate <- read_excel("data/RBA-cash-rate-august-2024.xlsx",sheet="Data")
+RBA_cash_rate <- RBA_cash_rate[-(1:11), -(3:18)]
+RBA_cash_rate[,2] <- lapply(RBA_cash_rate[,2],as.numeric)
+RBA_cash_rate[,1] <- lapply(RBA_cash_rate[,1],as.Date,"%d-%b-%Y")
+colnames(RBA_cash_rate) <- c("date","cash_rate")
+
+RBA_cash_rate_mod = RBA_cash_rate |>
+  dplyr::filter(
+    (month(date) == 3 | month(date) == 6 | month(date) == 9 | month(date) == 12) &
+      date > as.Date("2018-01-01")
+  ) |>
+  group_by(strftime(date,"%Y-%m")) |>
+  dplyr::filter(date == max(date)) |>
+  dplyr::mutate(
+    date = case_when(
+      month(date) == 3 ~ as.Date(sprintf("%s-03-31",year(date))),
+      month(date) == 6 ~ as.Date(sprintf("%s-06-30",year(date))),
+      month(date) == 9 ~ as.Date(sprintf("%s-09-30",year(date))),
+      month(date) == 12 ~ as.Date(sprintf("%s-12-31",year(date))),
+    )
+  )
+
+RBA_cash_rate_mod = RBA_cash_rate_mod[,-3]
+
+sales_df <- merge(x = sales_df,y=RBA_cash_rate_mod,by="date")
+rents_df <- merge(x = rents_df,y=RBA_cash_rate_mod,by="date")
+
+abs_earnings <- read_excel("data/ABS-weekly-earnings-trend.xlsx",sheet="Data1")
+abs_earnings <- abs_earnings[-(1:9),10]
+abs_earnings$date <- seq(as.Date('2012-07-01'), length.out=25, by='6 month') - days(1)
+colnames(abs_earnings) <- c("nsw_average_total_earnings","date")
+
+sales_df <- merge(sales_df,y = abs_earnings,by="date",all.x = TRUE)
+rents_df <- merge(rents_df,y = abs_earnings,by="date",all.x = TRUE)
 
 save(sales_df,file = "data/rdata/sales_df.RData")
 save(rents_df,file = "data/rdata/rents_df.RData")
+save(RBA_cash_rate, file = "data/rdata/rba_cash_rate.RData")
